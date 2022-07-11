509f5975d7eaa47a6d690ef70c3203bb97eb4e38
Walts03 <trungton3009@gmail.com>
2022-07-11 23:11:35 +0700

    FirstCommit

---
+++ Assets/LibraryHelper/Assets.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LibraryHelper {
+
+    /*
+     * Global Asset references
+     * Edit Asset references in the prefab CodeMonkey/Resources/CodeMonkeyAssets
+     * */
+    public class Assets : MonoBehaviour {
+
+        // Internal instance reference
+        private static Assets _i; 
+
+        // Instance reference
+        public static Assets i {
+            get { 
+                if (_i == null) _i = (Instantiate(Resources.Load("CodeMonkeyAssets")) as GameObject).GetComponent<Assets>(); 
+                return _i; 
+            }
+        }
+
+
+        // All references
+
+        public Sprite s_White;
+
+    }
+
+}
