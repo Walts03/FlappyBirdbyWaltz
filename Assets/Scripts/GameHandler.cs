7a196363cb61fdbb2b3ece5bc0c5e8582463d470
Walts03 <trungton3009@gmail.com>
2022-07-11 23:16:47 +0700

    second commit

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LibraryHelper;
+using LibraryHelper.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+        Score.Start();
+    }
+
+}
